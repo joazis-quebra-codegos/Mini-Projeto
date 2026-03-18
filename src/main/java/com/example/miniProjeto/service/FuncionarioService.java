98101b24ba6cb8851467c04affc5ca62fedfd748
JOÃO HENRIQUE DE SOUZA ZAMPARETTI <joao_zamparetti@estudante.sesisenai.org.br>
2026-03-18 20:21:11 -0300

    m

--- src/main/java/com/example/miniProjeto/service/FuncionarioService.java
+++ src/main/java/com/example/miniProjeto/service/FuncionarioService.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 @Service
 public class FuncionarioService {
+
     private final FuncionarioRepository repository;
 
     public FuncionarioService(FuncionarioRepository repository){
