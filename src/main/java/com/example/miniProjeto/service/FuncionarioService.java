0899494eb0774c7fda42cc84ca61ee0da442f5ae
KAIO GALVIN <kaio_galvin@estudante.sesisenai.org.br>
2026-03-18 20:14:43 -0300

    m

--- src/main/java/com/example/miniProjeto/service/FuncionarioService.java
+++ src/main/java/com/example/miniProjeto/service/FuncionarioService.java
@@ -1,8 +1,17 @@
 package com.example.miniProjeto.service;
 
+import com.example.miniProjeto.controller.FuncionarioController;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Service
 public class FuncionarioService {
+
+    private final FuncionarioService service;
+
+    public FuncionarioController(FuncionarioService service){this.service = service;}
+
+    @GetMapping
+
 
 }
