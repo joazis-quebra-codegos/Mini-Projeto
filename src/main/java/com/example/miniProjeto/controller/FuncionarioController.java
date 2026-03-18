5368c625c18212d0511cb62eb519f0d4ef785e5a
KAIO GALVIN <kaio_galvin@estudante.sesisenai.org.br>
2026-03-18 20:32:56 -0300

    funcionarioController

--- src/main/java/com/example/miniProjeto/controller/FuncionarioController.java
+++ src/main/java/com/example/miniProjeto/controller/FuncionarioController.java
@@ -1,11 +1,44 @@
 package com.example.miniProjeto.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.miniProjeto.model.Funcionario;
+import com.example.miniProjeto.service.FuncionarioService;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/Funcionarios")
 public class FuncionarioController {
 
+    private final FuncionarioService service;
+
+    public FuncionarioController(FuncionarioService service){this.service = service;}
+
+    @GetMapping
+    public List<Funcionario> listar(){
+        return service.listar();
+    }
+
+    @PostMapping
+    public Funcionario criar(@RequestBody @Valid Funcionario funcionario){
+        return service.salvar(funcionario);
+    }
+
+    @GetMapping("/{id}")
+    public Funcionario buscar(@PathVariable Long id){
+        return service.buscar(id);
+    }
+
+    @PutMapping("/{id}")
+    public Funcionario atualizar(@PathVariable Long id, @RequestBody @Valid Funcionario funcionario){
+        funcionario.setId(id);
+        return service.salvar(funcionario);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletar(@PathVariable Long id){
+        service.deletar(id);
+    }
 }
